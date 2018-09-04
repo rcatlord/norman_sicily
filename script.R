2b0ba12b8ba376de10e5ffbed01b361648f8f4cd
Henry Partridge <henry.partridge@trafford.gov.uk>
2018-09-04 09:56:14 +0100

    Update script.R

--- script.R
+++ script.R
@@ -4,18 +4,18 @@ library(tidyverse); library(sf) ; library(leaflet) ; library(leaflet.extras) ; l
 
 sf <- read_csv("data/sites.csv") %>% st_as_sf(coords = c("lon", "lat"), crs = 4326)
 
-popup <- paste0("<p style='text-align: center; font-weight: 400; line-height: 19px; font-family: \"Georgia\", serif; font-size: 15px;'><strong>", sf$name, ", (", sf$date, ")</strong><br/>",
+popup <- paste0("<p style='text-align: center; font-weight: 400; line-height: 19px; font-family: \"Georgia\", serif; font-size: 15px;'><em>", sf$detail, "</em><br/>",
                 "<a href='",  sf$url, "' target='_blank'><img style = 'height: 120px; padding-top:5px;' src = ", sf$image, "></a><br/><span style='color: #777;'>",
-                sf$location, "</span><br/>",
-                sf$detail, "</p>")
+                sf$name, ", (", sf$date, ") </span><br/>",
+                sf$location, "</p>")
 
 icon <- awesomeIcons(icon = "fa-map-marker", library = "fa", markerColor = "lightgreen")
 
 map <- leaflet() %>%
   setView(13.352104, 38.122561, zoom = 12) %>% 
-  addProviderTiles(providers$CartoDB.Positron, options = tileOptions(minZoom = 5, maxZoom = 17), group = "Road map") %>% 
-  addProviderTiles(providers$Esri.WorldImagery, options = tileOptions(minZoom = 5, maxZoom = 17), group = "Satellite") %>%
-  addProviderTiles(providers$Stamen.TerrainBackground, options = tileOptions(minZoom = 5, maxZoom = 17), group = "Terrain") %>%
+  addProviderTiles(providers$CartoDB.Positron, group = "Road map") %>% 
+  addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
+  addProviderTiles(providers$Stamen.TerrainBackground, group = "Terrain") %>%
   addAwesomeMarkers(data = sf, icon = icon, popup = popup,
                     label = ~name, labelOptions = labelOptions(opacity = 0),
                     group = 'sites') %>% 
